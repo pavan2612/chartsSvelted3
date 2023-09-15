c6999caed8985540c2992c4b7f5c6ea67313b706
Smasher <pavan2612.m@gmail.com>
2023-09-15 16:06:06 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  vite: {
+    server: {
+      hmr: {
+        protocol: "ws",
+        clientPort: 443,
+        strictPort: true,
+        overlay: false
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      $components: path.resolve("./src/components"),
+      $data: path.resolve("./src/data"),
+      $routes: path.resolve("./src/routes")
+    }
+  }
+});
